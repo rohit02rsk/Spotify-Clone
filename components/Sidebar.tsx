494023c8c77a13155e41c860664d3e74ddb223ef
S Rohit Kumar <rohitkumar.s2020@vitstudent.ac.in>
2023-07-29 22:39:44 +0530

    setup the app structure

---
+++ components/Sidebar.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import Box from "./Box";
+
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+
+interface SidebarProps {
+  children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  const pathname = usePathname();
+  const routes = useMemo(
+    () => [
+      {
+        icon: HiHome,
+        label: "Home",
+        active: pathname !== "/search",
+        href: "/",
+      },
+      {
+        icon: BiSearch,
+        label: "Search",
+        active: pathname === "/search",
+        href: "/search",
+      },
+    ],
+    [pathname]
+  );
+  return (
+    <div className="flex h-full">
+      <div
+        className="
+          hidden
+          md:flex
+          flex-col
+          gap-y-2
+          bg-black
+          h-full
+          w-[300px]
+          p-2
+        "
+      >
+        <Box>Sidebar Navigation</Box>
+        <Box>Song Library</Box>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
